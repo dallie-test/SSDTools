4484bc7da599a91b2d202ed9b48e7b5b3fcea423
rpfk <11407536+rpfk@users.noreply.github.com>
2019-03-13 22:17:51 +0100

    Call read file methods for various traffic files.
    Related to #18, #19, #20, #21, #22, #23, #24

--- tests/test_traffic.py
+++ tests/test_traffic.py
@@ -1,57 +1,76 @@
 import os
 
-import numpy as np
-import pandas as pd
 from gptools.traffic import Traffic
-
-
-def test_traffic_from_daisy_file_type_1():
-    # Create dummy data
-    df = pd.DataFrame(np.ones(6))
-
-    # Write dummy data to file
-    df.to_csv('data/df.csv')
-
-    # Create a traffic object from the dummy data file
-    traffic = Traffic.from_daisy_file_type_1('data/df.csv')
-
-    # Check if the content is correct
-    np.testing.assert_equal(df.values, traffic.data.values)
 
 
 def test_traffic_from_daisy_file_type_1_winter_season():
     # Get the path to the Daisy file
-    file_path = abs_path('tests/data/traffic Winterseizoen.txt')
+    file_path = abs_path('data/traffic Winterseizoen.txt')
+
+    # Create a traffic object from the data file
+    traffic = Traffic.from_daisy_file_type_1(file_path)
+
+    pass
 
 
 def test_traffic_from_daisy_file_type_2_summer_season():
     # Get the path to the Daisy file
-    file_path = abs_path('tests/data/traffic Zomerseizoen.txt')
+    file_path = abs_path('data/traffic Zomerseizoen.txt')
+
+    # Create a traffic object from the data file
+    traffic = Traffic.from_daisy_file_type_2(file_path)
+
+    pass
 
 
 def test_traffic_from_daisy_file_type_3_meteo_years_runways():
     # Get the path to the Daisy file
-    file_path = abs_path('tests/data/traffic 1971-2016 - years.txt')
+    file_path = abs_path('data/traffic 1971-2016 - years.txt')
+
+    # Create a traffic object from the data file
+    traffic = Traffic.from_daisy_file_type_3(file_path)
+
+    pass
 
 
 def test_traffic_from_daisy_file_type_4_runway_combinations():
     # Get the path to the Daisy file
-    file_path = abs_path('tests/data/traffic 1971-2016 - pref.txt')
+    file_path = abs_path('data/traffic 1971-2016 - pref.txt')
+
+    # Create a traffic object from the data file
+    traffic = Traffic.from_daisy_file_type_4(file_path)
+
+    pass
 
 
 def test_traffic_from_daisy_file_type_5_mean():
     # Get the path to the Daisy file
-    file_path = abs_path('tests/data/traffic 1971-2016 - mean.txt')
+    file_path = abs_path('data/traffic 1971-2016 - mean.txt')
+
+    # Create a traffic object from the data file
+    traffic = Traffic.from_daisy_file_type_5(file_path)
+
+    pass
 
 
 def test_traffic_from_casper_file():
     # Get the path to the Casper file
-    file_path = abs_path('tests/data/Vluchten Export 2017-11-01 00_00_00 - 2018-11-01 00_00_00_2019-01-29 10_59_35.csv')
+    file_path = abs_path('data/Vluchten Export 2017-11-01 00_00_00 - 2018-11-01 00_00_00_2019-01-29 10_59_35.csv')
+
+    # Create a traffic object from the data file
+    traffic = Traffic.from_casper_file(file_path)
+
+    pass
 
 
 def test_traffic_from_nlr_file():
     # Get the path to the NLR file
-    file_path = abs_path('tests/data/20181107_Traffic_2018.xlsx')
+    file_path = abs_path('data/20181107_Traffic_2018.xlsx')
+
+    # Create a traffic object from the data file
+    traffic = Traffic.from_nlr_file(file_path)
+
+    pass
 
 
 def abs_path(rel_path):
