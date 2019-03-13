b9f3a1a4138499bf92506533c031ebefe23a1702
rpfk <11407536+rpfk@users.noreply.github.com>
2019-03-13 17:52:58 +0100

    Mark the various traffic files according to the description from the meeting on March 12.
    Relates to #7

--- tests/test_traffic.py
+++ tests/test_traffic.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import pandas as pd
 from gptools.traffic import Traffic
@@ -15,3 +17,42 @@ def test_traffic_from_daisy_file_type_1():
 
     # Check if the content is correct
     np.testing.assert_equal(df.values, traffic.data.values)
+
+
+def test_traffic_from_daisy_file_type_1_winter_season():
+    # Get the path to the Daisy file
+    file_path = abs_path('tests/data/traffic Winterseizoen.txt')
+
+
+def test_traffic_from_daisy_file_type_2_summer_season():
+    # Get the path to the Daisy file
+    file_path = abs_path('tests/data/traffic Zomerseizoen.txt')
+
+
+def test_traffic_from_daisy_file_type_3_meteo_years_runways():
+    # Get the path to the Daisy file
+    file_path = abs_path('tests/data/traffic 1971-2016 - years.txt')
+
+
+def test_traffic_from_daisy_file_type_4_runway_combinations():
+    # Get the path to the Daisy file
+    file_path = abs_path('tests/data/traffic 1971-2016 - pref.txt')
+
+
+def test_traffic_from_daisy_file_type_5_mean():
+    # Get the path to the Daisy file
+    file_path = abs_path('tests/data/traffic 1971-2016 - mean.txt')
+
+
+def test_traffic_from_casper_file():
+    # Get the path to the Casper file
+    file_path = abs_path('tests/data/Vluchten Export 2017-11-01 00_00_00 - 2018-11-01 00_00_00_2019-01-29 10_59_35.csv')
+
+
+def test_traffic_from_nlr_file():
+    # Get the path to the NLR file
+    file_path = abs_path('tests/data/20181107_Traffic_2018.xlsx')
+
+
+def abs_path(rel_path):
+    return os.path.join(os.path.dirname(__file__), rel_path)
