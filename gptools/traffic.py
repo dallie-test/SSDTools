d8109f01f2fbd0e1b76cea59eab582c0d8f56bad
rpfk <11407536+rpfk@users.noreply.github.com>
2019-03-26 18:02:45 +0100

    Add placeholder for the runway usage function. Add the functions for the seasons calculations.

--- gptools/traffic.py
+++ gptools/traffic.py
@@ -1,4 +1,7 @@
+import datetime
+
 import xlrd
+import numpy as np
 import pandas as pd
 
 
@@ -61,3 +64,67 @@ class Traffic(object):
 
         # Return the traffic object
         return cls(data_frame)
+
+
+def runway_usage(traffic, period):
+    """
+    Aggregate the runway usage and calculate the mean, median, minimum, maximum and standard deviation
+
+    :param Traffic traffic:
+    :param str period: a regular expression for the period, e.g. 'D' or 'D|E|N'
+    """
+
+    # todo: Require the following columns d_lt, d_runway, d_period, d_den, d_myear en total
+
+
+def start_summer_season(year):
+    """
+    Determine the start of the summer season, which is the last Sunday of the month March.
+    Valid between 1900 and 2099.
+    source: http://delphiforfun.org/Programs/Math_Topics/DSTCalc.htm
+
+    :param int year: the calendar year of the season
+    :return the start date of the summer season
+    :rtype datetime.date
+    """
+    if year > 2099 or year < 1900:
+        raise ValueError('This method is only valid for years between 1900 and 2099.')
+    return datetime.date(year, 3, 31 - np.round((4 + 5 * year / 4), 0) % 7)
+
+
+def start_winter_season(year):
+    """
+    Determine the start of the summer season, which is the last Sunday of the month October.
+    Valid between 1900 and 2099.
+    source: http://delphiforfun.org/Programs/Math_Topics/DSTCalc.htm
+
+    :param int year: the calendar year of the season
+    :return the start date of the summer season
+    :rtype datetime.date
+    """
+    if year > 2099 or year < 1900:
+        raise ValueError('This method is only valid for years between 1900 and 2099.')
+    return datetime.date(year, 10, 31 - np.round((1 + 5 * year / 4), 0) % 7)
+
+
+def get_year_of_use(year):
+    """
+    Determine start- and end date, number of days in the year of use
+    """
+
+    # Create a dictionary for the information
+    year_info = {
+        'year': year,
+        'start_summer': start_summer_season(year),
+        'end_summer': start_winter_season(year) + datetime.timedelta(-1),
+        'start_winter': start_winter_season(year - 1),
+        'end_winter': start_summer_season(year)
+    }
+
+    # Number of days, weeks
+    year_info['winter_days'] = (year_info['end_winter'] - year_info['start_winter']).days + 1
+    year_info['summer_days'] = (year_info['end_summer'] - year_info['end_winter']).days
+    year_info['winter_weeks'] = year_info['winter_days'] / 7
+    year_info['summer_weeks'] = year_info['summer_days'] / 7
+
+    return year_info
