902226d7729274c3ab215157e266177e350d3c2d
rpfk <11407536+rpfk@users.noreply.github.com>
2019-03-30 12:38:43 +0100

    Replaced method for calculating the season start by using the datetime weekday method. Removes limiting scope of the previous method.

--- tests/test_traffic_seasons.py
+++ tests/test_traffic_seasons.py
@@ -8,58 +8,46 @@ from gptools.traffic import get_year_of_use
 
 
 def test_start_summer_nominal():
-    nominal = np.arange(1996, 2099, 1)
+    nominal = np.arange(1900, 2100)
     for y in nominal:
         summer_start = start_summer_season(y)
         assert isinstance(summer_start, datetime.date)
-    pass
+        assert summer_start.weekday() == 6
+
+
+def test_start_summer_nominal_2019():
+    assert start_summer_season(2019) == datetime.date(2019, 3, 31)
+
+
+def test_start_summer_nominal_2018():
+    assert start_summer_season(2018) == datetime.date(2018, 3, 25)
 
 
 @raises(TypeError)
 def test_start_summer_type():
     # Type error expected for float input
-    summer_start = start_summer_season(2000.)
-    assert False
-
-
-@raises(ValueError)
-def test_start_summer_edge_1995():
-    # Value error expected when out of bounds case is input
-    summer_start = start_summer_season(1995)
-    assert False
-
-
-@raises(ValueError)
-def test_start_summer_edge_2100():
-    # Value error expected when out of bounds case is input
-    summer_start = start_summer_season(2100)
-    assert False
+    start_summer_season(2000.)
 
 
 def test_start_winter_nominal():
-    nominal = np.arange(1996, 2099, 1)
+    nominal = np.arange(1900, 2100)
     for y in nominal:
         winter_start = start_winter_season(y)
         assert isinstance(winter_start, datetime.date)
-    pass
+        assert winter_start.weekday() == 6
+
+
+def test_start_winter_nominal_2019():
+    assert start_winter_season(2019) == datetime.date(2019, 10, 27)
+
+
+def test_start_winter_nominal_2018():
+    assert start_winter_season(2018) == datetime.date(2018, 10, 28)
 
 
 @raises(TypeError)
 def test_start_winter_type():
-    winter_start = start_winter_season(2000.)
-    assert False
-
-
-@raises(ValueError)
-def test_start_winter_edge_1995():
-    winter_start = start_winter_season(1995)
-    assert False
-
-
-@raises(ValueError)
-def test_start_winter_edge_2100():
-    winter_start = start_winter_season(2100)
-    assert False
+    start_winter_season(2000.)
 
 
 def test_year_of_use_nominal():
@@ -67,22 +55,8 @@ def test_year_of_use_nominal():
     for y in nominal:
         info = get_year_of_use(y)
         assert isinstance(info, dict)
-    pass
-
-
-@raises(ValueError)
-def test_year_of_use_edge_1996():
-    info = get_year_of_use(1996)
-    assert False
-
-
-@raises(ValueError)
-def test_year_of_use_edge_2100():
-    info = get_year_of_use(2100)
-    assert False
 
 
 @raises(TypeError)
 def test_year_of_use_type():
-    info = get_year_of_use(2000.)
-    assert False
+    get_year_of_use(2000.)
