4e6133e8468b9f978f7ce89282c7964da0b780a7
rpfk <11407536+rpfk@users.noreply.github.com>
2019-03-30 21:25:51 +0100

    Add methods for sleep disturbance and annoyance to the WBS object.

--- gptools/wbs.py
+++ gptools/wbs.py
@@ -1,4 +1,10 @@
+import copy
+import numpy as np
 import pandas as pd
+
+from warnings import warn
+
+from gptools.grid import Grid
 
 
 class WBS(object):
@@ -18,3 +24,118 @@ class WBS(object):
 
         # Return the traffic object
         return cls(data_frame)
+
+    def copy(self):
+        return copy.deepcopy(self)
+
+    def add_noise_from_grid(self, grid):
+        """
+        Calculate the noise levels for each residence by interpolating the grid results.
+
+        :param Grid grid:
+        """
+
+        # Get the interpolation function
+        interpolation = grid.interpolation_function()
+
+        # Set the interpolated noise levels for each wbs location
+        self.data[grid.unit] = interpolation(self.data['y'], self.data['x'], grid=False)
+
+        return self
+
+    def select_above(self, level, unit):
+        return self.data[unit] >= level
+
+    def count_above(self, level, unit):
+        return self.select_above(level, unit).sum()
+
+    def count_homes_above(self, level, unit):
+        return self.data.loc[self.select_above(level, unit), 'woningen'].sum()
+
+    def count_annoyed_people(self, threshold=48):
+        # Get the rows above the Lden threshold
+        data = self.data[self.select_above(threshold, 'Lden')]
+
+        # Calculate the relative annoyance for each residence
+        relative_annoyance = annoyance(data['Lden'])
+
+        # Multiply the relative annoyance by the number of people
+        return (data['personen'] * relative_annoyance).sum()
+
+    def count_sleep_disturbed_people(self, threshold=40):
+        # Get the rows above the Lnight threshold
+        data = self.data[self.select_above(threshold, 'Lnight')]
+
+        # Calculate the relative sleep disturbance for each residence
+        relative_sleep_disturbance = sleep_disturbance(data['Lnight'])
+
+        # Multiply the relative sleep disturbance by the number of people
+        return (data['personen'] * relative_sleep_disturbance).sum()
+
+
+def annoyance(noise_levels, de='doc29', max_noise_level=None):
+    """
+    Calculate the relative annoyance for each noise level value. This particular method is only valid for Lden noise
+    levels.
+
+    Two dose-effect relationships are supported:
+    1) doc29: the newest relationship, to be used for calculations with ECAC Doc. 29
+    2) ges2002: an older dose-effect relationship.
+
+    This method also supports a cut-off at a specified dB value. For doc29 it is not common to use a cut-off, for
+    ges2002 it is customary to apply a cut-off at 65dB(A).
+
+    :param np.ndarray|pd.Series noise_levels: the noise levels for which to calculate the relative annoyance.
+    :param str de: the dose-effect relationship to apply, defaults to 'doc29'.
+    :param float max_noise_level: the cut-off noise level.
+    :return: the relative annoyance for the provided noise levels.
+    """
+
+    # Apply a cut-off at max_db if provided
+    if max_noise_level is not None:
+        noise_levels[noise_levels > max_noise_level] = max_noise_level
+
+    # Apply the dose-effect relationship
+    if de == 'ges2002':
+        return 1 / (1 / np.exp(-8.1101 + 0.1333 * noise_levels) + 1)
+    elif de == 'doc29':
+        if max_noise_level is not None:
+            warn('You have set max_db to {} dB(A) while using the doc29 dose-effect relationship. However, for ' +
+                 'doc29 it is not common to use a cut-off.'.format(max_noise_level), UserWarning)
+        return 1 - 1 / (1 + np.exp(-7.7130 + 0.1260 * noise_levels))
+
+    raise ValueError('The provided dose-effect relationship {} is not know. Please use ges2002 or doc29.'.format(de))
+
+
+def sleep_disturbance(noise_levels, de='doc29', max_noise_level=None):
+    """
+    Calculate the relative sleep disturbance for each noise level value. This particular method is only valid for Lnight
+    noise levels.
+
+    Two dose-effect relationships are supported:
+    1) doc29: the newest relationship, to be used for calculations with ECAC Doc. 29
+    2) ges2002: an older dose-effect relationship.
+
+    This method also supports a cut-off at a specified dB value. For doc29 it is not common to use a cut-off, for
+    ges2002 it is customary to apply a cut-off at 57dB(A).
+
+    :param np.ndarray|pd.Series noise_levels: the noise levels for which to calculate the relative sleep disturbance.
+    :param str de: the dose-effect relationship to apply, defaults to 'doc29'.
+    :param float max_noise_level: the cut-off noise level.
+    :return: the relative sleep disturbance for the provided noise levels.
+    """
+
+    # Apply a cut-off at max_db if provided
+    if max_noise_level is not None:
+        noise_levels[noise_levels > max_noise_level] = max_noise_level
+
+    # Apply the dose-effect relationship
+    if de == 'ges2002':
+        return 1 / (1 / np.exp(-6.642 + 0.1046 * noise_levels) + 1)
+    elif de == 'doc29':
+        if max_noise_level is not None:
+            warn('You have set max_db to {} dB(A) while using the doc29 dose-effect relationship. However, for ' +
+                 'doc29 it is not common to use a cut-off.'.format(max_noise_level), UserWarning)
+        return 1 - 1 / (1 + np.exp(-6.2952 + 0.0960 * noise_levels))
+
+    raise ValueError('The provided dose-effect relationship {} is not know. Please use ges2002 or doc29.'.format(de))
--- tests/test_wbs.py
+++ tests/test_wbs.py
@@ -1,5 +1,8 @@
 import os
+import numpy as np
+from nose.tools import raises
 
+from gptools.grid import Grid
 from gptools.wbs import WBS
 
 
@@ -8,9 +11,181 @@ def test_wbs_read_file():
     file_path = abs_path('data/wbs2005.h5')
 
     # Create a wbs object from the data file
+    WBS.read_file(file_path)
+
+
+def test_add_noise_from_grid():
+    # Get the path to the WBS file
+    file_path = abs_path('data/wbs2005.h5')
+
+    # Create a wbs object from the data file
     wbs = WBS.read_file(file_path)
 
-    pass
+    # Get the path to the Envira file
+    file_path = abs_path('data/MER2015 - Doc29 - Lden y1974.dat')
+
+    # Create a grid object from the data file
+    grid = Grid.read_envira(file_path)
+
+    # Add the interpolated Lden values from the grid
+    wbs.add_noise_from_grid(grid)
+
+
+def test_add_noise_from_grid_lden_lnight():
+    # Get the path to the WBS file
+    file_path = abs_path('data/wbs2005.h5')
+
+    # Create a wbs object from the data file
+    wbs = WBS.read_file(file_path)
+
+    # Get the path to the Lden Envira file
+    file_path = abs_path('data/MER2015 - Doc29 - Lden y1974.dat')
+
+    # Create a grid object from the data file
+    grid = Grid.read_envira(file_path)
+
+    # Add the interpolated Lden values from the grid
+    wbs.add_noise_from_grid(grid)
+
+    # Get the path to the Lnight Envira file
+    file_path = abs_path('data/GP2018 - Lnight y2016.dat')
+
+    # Create a grid object from the data file
+    grid = Grid.read_envira(file_path)
+
+    # Add the interpolated Lnight values from the grid
+    wbs.add_noise_from_grid(grid)
+
+
+def test_select_above():
+    # Get the path to the WBS file
+    file_path = abs_path('data/wbs2005.h5')
+
+    # Create a wbs object from the data file
+    wbs = WBS.read_file(file_path)
+
+    # Get the path to the Envira file
+    file_path = abs_path('data/MER2015 - Doc29 - Lden y1974.dat')
+
+    # Create a grid object from the data file
+    grid = Grid.read_envira(file_path)
+
+    # Add the interpolated Lden values from the grid
+    wbs.add_noise_from_grid(grid)
+
+    # Select the Lden values above 40
+    s = wbs.select_above(40, 'Lden').values
+
+    assert isinstance(s, np.ndarray)
+
+
+@raises(KeyError)
+def test_select_above_wrong_unit():
+    # Get the path to the WBS file
+    file_path = abs_path('data/wbs2005.h5')
+
+    # Create a wbs object from the data file
+    wbs = WBS.read_file(file_path)
+
+    # Get the path to the Envira file
+    file_path = abs_path('data/MER2015 - Doc29 - Lden y1974.dat')
+
+    # Create a grid object from the data file
+    grid = Grid.read_envira(file_path)
+
+    # Add the interpolated Lden values from the grid
+    wbs.add_noise_from_grid(grid)
+
+    # Select the Lnight values above 48
+    wbs.select_above('Lnight', 40)
+
+
+def test_count_above():
+    # Get the path to the WBS file
+    file_path = abs_path('data/wbs2005.h5')
+
+    # Create a wbs object from the data file
+    wbs = WBS.read_file(file_path)
+
+    # Get the path to the Envira file
+    file_path = abs_path('data/MER2015 - Doc29 - Lden y1974.dat')
+
+    # Create a grid object from the data file
+    grid = Grid.read_envira(file_path)
+
+    # Add the interpolated Lden values from the grid
+    wbs.add_noise_from_grid(grid)
+
+    # Select the Lden values above 40
+    s = wbs.count_above(40, 'Lden')
+
+    assert isinstance(s, np.int64)
+
+
+def test_count_homes_above():
+    # Get the path to the WBS file
+    file_path = abs_path('data/wbs2005.h5')
+
+    # Create a wbs object from the data file
+    wbs = WBS.read_file(file_path)
+
+    # Get the path to the Envira file
+    file_path = abs_path('data/MER2015 - Doc29 - Lden y1974.dat')
+
+    # Create a grid object from the data file
+    grid = Grid.read_envira(file_path)
+
+    # Add the interpolated Lden values from the grid
+    wbs.add_noise_from_grid(grid)
+
+    # Count the homes with Lden values above 40
+    s = wbs.count_homes_above(40, 'Lden')
+
+    assert isinstance(s, float)
+
+
+def test_count_annoyed_people():
+    # Get the path to the WBS file
+    file_path = abs_path('data/wbs2005.h5')
+
+    # Create a wbs object from the data file
+    wbs = WBS.read_file(file_path)
+
+    # Get the path to the Envira file
+    file_path = abs_path('data/MER2015 - Doc29 - Lden y1974.dat')
+
+    # Create a grid object from the data file
+    grid = Grid.read_envira(file_path)
+
+    # Add the interpolated Lden values from the grid
+    wbs.add_noise_from_grid(grid)
+
+    # Count the annoyed people
+    s = wbs.count_annoyed_people()
+
+    assert isinstance(s, float)
+
+
+def test_count_sleep_disturbed_people():
+    # Get the path to the WBS file
+    file_path = abs_path('data/wbs2005.h5')
+
+    # Create a wbs object from the data file
+    wbs = WBS.read_file(file_path)
+
+    # Get the path to the Envira file
+    file_path = abs_path('data/GP2018 - Lnight y2016.dat')
+
+    # Create a grid object from the data file
+    grid = Grid.read_envira(file_path)
+
+    # Add the interpolated Lnight values from the grid
+    wbs.add_noise_from_grid(grid)
+
+    # Count the sleep disturbed people
+    s = wbs.count_sleep_disturbed_people()
+
+    assert isinstance(s, float)
 
 
 def abs_path(rel_path):
