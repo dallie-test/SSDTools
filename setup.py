75d4677bcc90d1142e44b25277095e5cb9993302
rpfk <11407536+rpfk@users.noreply.github.com>
2019-04-17 09:35:48 +0200

    Add missing packages to the list of required packages.

--- setup.py
+++ setup.py
@@ -41,7 +41,8 @@ setup(
     license='None',
     packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
     include_package_data=True,
-    install_requires=['pandas', 'numpy', 'scipy', 'xlrd', 'tables', 'matplotlib', 'pyshp', 'requests'],
+    install_requires=['pandas', 'numpy', 'scipy', 'xlrd', 'tables', 'matplotlib', 'pyshp', 'requests', 'geopandas',
+                      'descartes'],
     zip_safe=False,
     project_urls={
         'Source': 'https://github.com/schiphol-Hub/GPTools/',
