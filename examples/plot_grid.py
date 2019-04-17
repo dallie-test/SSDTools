8dd9ba0fc5d6445db225f9bf5b5a75536909a89f
rpfk <11407536+rpfk@users.noreply.github.com>
2019-04-17 23:49:20 +0200

    Add example for comparison heatmap and add colorbars.

--- examples/plot_grid.py
+++ examples/plot_grid.py
@@ -68,8 +68,40 @@ def example_3():
     # Add the heatmap
     plot.add_heatmap(vmin=46, vmax=70)
 
+    # Add a colorbar
+    plot.add_colorbar()
+
     # Save the figure
     plot.save('figures/plot_grid_example_3.pdf')
+
+    # And show the plot
+    plot.show()
+
+
+def example_4():
+    # Collect a Grid
+    grid_1 = Grid.read_envira('../tests/data/MER2019 H_500_doc29_VVR/MER2018 - Doc29 - Lden y1974.dat')
+
+    # Collect a Grid to compare
+    grid_2 = Grid.read_envira('../tests/data/H_500_00_doc29/MER2015 - Doc29 - Lden y1974.dat')
+
+    # Create a figure
+    plot = GridPlot(grid_1)
+
+    # Add the background
+    plot.add_background('../lib/Schiphol_RD900dpi.png')
+
+    # Add a scale
+    plot.add_scale()
+
+    # Add the comparison heatmap
+    plot.add_comparison_heatmap(grid_2, vmin=-3, vmax=3)
+
+    # Add a colorbar
+    plot.add_colorbar()
+
+    # Save the figure
+    plot.save('figures/plot_grid_example_4.pdf')
 
     # And show the plot
     plot.show()
@@ -79,3 +111,4 @@ if __name__ == "__main__":
     example_1()
     example_2()
     example_3()
+    example_4()
