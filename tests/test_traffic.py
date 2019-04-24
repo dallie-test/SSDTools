4713ed8a848efb3afeffca975aa834f85e623542
rpfk <11407536+rpfk@users.noreply.github.com>
2019-04-24 16:21:08 +0200

    Add DEN and DENEM distributions to the examples.

--- tests/test_traffic.py
+++ tests/test_traffic.py
@@ -8,7 +8,10 @@ def test_read_daisy_phase_file():
     file_path = abs_path('data/traffic Winterseizoen.txt')
 
     # Create a traffic object from the data file
-    traffic = Traffic.read_daisy_phase_file(file_path)
+    aggregate = Traffic.read_daisy_phase_file(file_path)
+
+    # Get the DEN distribution for takeoff and landing rounded by 100
+    distribution = aggregate.get_den_distribution(separate_by='d_lt').round(decimals=-2)
 
     pass
 
@@ -18,7 +21,10 @@ def test_read_daisy_phase_file_summer_season():
     file_path = abs_path('data/traffic Zomerseizoen.txt')
 
     # Create a traffic object from the data file
-    traffic = Traffic.read_daisy_phase_file(file_path)
+    aggregate = Traffic.read_daisy_phase_file(file_path)
+
+    # Get the DEN distribution for takeoff and landing rounded by 100
+    distribution = aggregate.get_den_distribution(separate_by='d_lt')
 
     pass
 
@@ -47,13 +53,21 @@ def test_read_daisy_meteoyear_file():
 
     # todo: Add a test with data from Matlab
 
+    # Get the DEN distribution for each meteo year
+    distribution = aggregate.get_den_distribution(separate_by='d_myear')
+
+    pass
+
 
 def test_read_daisy_runway_combination_file():
     # Get the path to the Daisy file
     file_path = abs_path('data/traffic 1971-2016 - pref.txt')
 
     # Create a traffic object from the data file
-    traffic = Traffic.read_daisy_runway_combination_file(file_path)
+    aggregate = Traffic.read_daisy_runway_combination_file(file_path)
+
+    # Get the DEN distribution rounded by 100
+    distribution = aggregate.get_den_distribution().round(decimals=-2)
 
     pass
 
@@ -63,7 +77,10 @@ def test_read_daisy_mean_file():
     file_path = abs_path('data/traffic 1971-2016 - mean.txt')
 
     # Create a traffic object from the data file
-    traffic = Traffic.read_daisy_mean_file(file_path)
+    aggregate = Traffic.read_daisy_mean_file(file_path)
+
+    # Get the DEN distribution for takeoff and landing rounded by 100
+    distribution = aggregate.get_den_distribution(separate_by='d_lt').round(decimals=-2)
 
     pass
 
@@ -75,6 +92,12 @@ def test_traffic_from_casper_file():
     # Create a traffic object from the data file
     traffic = Traffic.from_casper_file(file_path)
 
+    # Add day (D), evening (E), night (N)
+    traffic.add_den()
+
+    # Get the DEN distribution rounded by 25
+    distribution = (25 * (traffic.get_den_distribution() / 25).round()).astype(int)
+
     pass
 
 
@@ -85,7 +108,53 @@ def test_traffic_from_nlr_file():
     # Create a traffic object from the data file
     traffic = Traffic.from_nlr_file(file_path)
 
+    # Add day (D), evening (E), night (N)
+    traffic.add_den()
+
+    # Get the DEN distribution rounded by 5
+    distribution = (5 * (traffic.get_den_distribution() / 5).round()).astype(int)
+
     pass
+
+
+def test_traffic_add_season():
+    # Get the path to the Casper file
+    file_path = abs_path('data/Vluchten Export 2017-11-01 00_00_00 - 2018-11-01 00_00_00_2019-01-29 10_59_35.csv')
+
+    # Create a traffic object from the data file
+    traffic = Traffic.from_casper_file(file_path)
+
+    # Add the seasons to the data
+    traffic.add_season()
+
+    # Check if the season is present in the traffic data
+    assert 'season' in traffic.data
+
+    # Check if it has winter and summer in it
+    assert ['summer', 'winter'] == traffic.data['season'].sort_values().unique().tolist()
+
+
+def test_traffic_get_denem_distribution():
+    # Get the path to the Casper file
+    file_path = abs_path('data/Vluchten Export 2017-11-01 00_00_00 - 2018-11-01 00_00_00_2019-01-29 10_59_35.csv')
+
+    # Create a traffic object from the data file
+    traffic = Traffic.from_casper_file(file_path)
+
+    # Add the seasons to the data
+    traffic.add_season()
+
+    # Add the departure/arrival to the data
+    traffic.add_takeoff_landing()
+
+    # Add the day (D), evening (E), night (N), and early morning (EM) to the data
+    traffic.add_denem()
+
+    # Get the day (D), evening (E), night (N), and early morning (EM) distribution
+    distribution = traffic.get_denem_distribution()
+
+    # Check if it has the requested shape
+    assert distribution.shape == (4, 2)
 
 
 def abs_path(rel_path):
