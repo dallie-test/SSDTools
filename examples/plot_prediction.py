dae11d0b699f9c4cd977d339958128edb5d9e674
rpfk <11407536+rpfk@users.noreply.github.com>
2019-04-26 22:15:45 +0200

    Add example of a prediction plot

---
+++ examples/plot_prediction.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from gptools.figures import plot_prediction
+from matplotlib import rc
+
+if __name__ == "__main__":
+    # Set the branding to use
+    rc('font', family='Arial', size=13)
+    rc('lines', linewidth=2.5, markersize=8)
+    rc('axes.spines', left=False, bottom=False, right=False, top=False)
+
+    # Create some random historic data
+    historic_years = pd.Series(np.arange(2001, 2019), name='years')
+    historic_data = pd.Series(np.arange(0, 18) / 2 + np.random.rand(18), name='data')
+    history = pd.concat([historic_years, historic_data], axis=1)
+
+    # Create some random predicted data
+    predicted_year = pd.Series(2020 + np.zeros(20, dtype=int), name='years')
+    predicted_data = pd.Series(9 + 2 * np.random.rand(20), name='data')
+    prediction_2020 = pd.concat([predicted_year, predicted_data], axis=1)
+
+    predicted_year = pd.Series(2025 + np.zeros(20, dtype=int), name='years')
+    predicted_data = pd.Series(9 + 5 * np.random.rand(20), name='data')
+    prediction_2025 = pd.concat([predicted_year, predicted_data], axis=1)
+
+    # Create a prediction plot
+    fig, ax = plot_prediction(history, pd.concat([prediction_2020, prediction_2025], axis=0),
+                              history_name='realisatie', prediction_name='prognose')
+
+    plt.savefig('figures/plot_prediction.pdf')
+
+    plt.show()
