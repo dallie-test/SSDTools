b718f3614c0831e8dcb5a4bf505fb69f7372d7d7
rpfk <11407536+rpfk@users.noreply.github.com>
2019-04-27 16:43:30 +0200

    Use Schiphol colors in the color cycler and add an example.

---
+++ examples/plot_branding.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    # Set the branding to use
+    plt.style.use('../gptools/branding/schiphol_default.rc')
+
+    # Plot the various colors as bars
+    for x in range(11):
+        plt.bar(x, 10, label='Color {}'.format(x))
+
+    # Add a legend to show the labels
+    plt.legend()
+
+    # Show the figure
+    plt.show()
