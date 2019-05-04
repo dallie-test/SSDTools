3d27f40ce1d61652248bb6fef11167674361f115
rpfk <11407536+rpfk@users.noreply.github.com>
2019-05-04 14:39:22 +0200

    Removed legacy files from the repository

--- GPtools_matlab/external/SVG/SVGpath_format.m
+++
@@ -1,8 +0,0 @@
-function [SVGtext] = SVGpath_format(col, dB)
-% Maak path format string voor flood2SVG
-
-[color transp] = SVGrgb(col, dB);
-
-% maak SVGtekst
-SVGtext = sprintf('\n\t<!-- Contour %4.1f dB(A) -->\n',dB);
-SVGtext= [SVGtext sprintf('\t<path style="fill:%s;fill-opacity:%4.2f;fill-rule:evenodd;stroke:none"\n',color, transp)];
--- GPtools_matlab/sjablonen/change_font.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sed -i 's/sans-serif/Myriad Pro/g' *.svgt
-sed -i 's/Schiphol Frutiger/Myriad Pro/g' *.svgt
