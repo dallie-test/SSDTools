7a17e7e16f20ba3793aa752e6c9918892205c53a
rpfk <11407536+rpfk@users.noreply.github.com>
2019-05-07 10:06:55 +0200

    Removed git versioning due to errors with Git hash.

--- ssdtools/utils/version.py
+++ ssdtools/utils/version.py
@@ -1,7 +1,3 @@
-import os
-import subprocess
-
-
 def git_version():
     """
     Return the git revision as a string.
@@ -10,30 +6,7 @@ def git_version():
     :rtype str
     """
 
-    def _minimal_ext_cmd(cmd):
-        # construct minimal environment
-        env = {}
-        for k in ['SYSTEMROOT', 'PATH', 'HOME']:
-            v = os.environ.get(k)
-            if v is not None:
-                env[k] = v
-        # LANGUAGE is used on win32
-        env['LANGUAGE'] = 'C'
-        env['LANG'] = 'C'
-        env['LC_ALL'] = 'C'
-
-        # Set the working directory
-        cwd = os.path.dirname(os.path.abspath(__file__))
-
-        return subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env, cwd=cwd).communicate()[0]
-
-    try:
-        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
-        GIT_REVISION = out.strip().decode('ascii')
-    except OSError:
-        GIT_REVISION = "Unknown"
-
-    return GIT_REVISION
+    return "Unknown"
 
 
 def get_version(version=None):
